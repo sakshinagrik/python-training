c387659a9df0831fe7c7e108015b9f114ebee5a1
24510290055 <nagrik@4321>
2026-05-30 12:01:50 +0530

    Day 3:for loop,list,function

---
+++ list.py
@@ -0,0 +1,16 @@
+print("Student list")
+student = ["John","Jane","Doe","Smith","Shashank"]
+print("1",student[0])
+print("2",student[1])
+print("3",student[2])   
+print("4",student[3])
+print("5",student[4])
+
+for students in student:
+    print(students)
+def Function_name(name):
+    print(f"Hello {name}!")
+Function_name("Shashank")
+print("***********************************")
+for students in student:
+   print(students)
