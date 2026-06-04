0cbc89880d8df26e145c1103246b998bd387624b
24510290055 <nagrik@4321>
2026-06-04 22:44:29 +0530

    Added Flask templets and app

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask,render_template
 
 apna_nam = Flask(__name__)
+stud = [
+    {"name": "Tanuja", "roll": 1, "marks": 85},
+    {"name": "Pratiksha", "roll": 2, "marks": 78},
+    {"name": "Shlok", "roll": 3, "marks": 92},
+    {"name": "Lucky", "roll": 4, "marks": 65},
+]
 
 @apna_nam.route('/')
 def home():
-
-    return '<h1>Welcome to my Flask App!</h1>'
+    return render_template('Home.html',students=stud)
+@apna_nam.route('/about')
+def about():
+    return render_template('About.html')
+@apna_nam.route('/students')
+def students():
+    return  render_template('Student.html',students=stud)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    print("Inside main")
+    apna_nam.run(debug=True)
+
